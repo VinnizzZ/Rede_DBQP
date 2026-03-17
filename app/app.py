0eba86759434ae67b33c21b7fe35c8d55e17a9a4
Vinicius Binda <vinnibinda@hotmail.com>
2026-03-17 19:15:10 -0300

    configurações iniciais .env e banco de dados

--- app/app.py
+++ app/app.py
@@ -1,9 +1,21 @@
+# Imports externos
+import os
 from flask import Flask, render_template, request, jsonify, redirect, url_for
+from dotenv import load_dotenv
+
+# Imports internos
 from routes.auth import auth_bp
 from routes.posts import posts_bp
 from routes.users import users_bp
 
 app = Flask(__name__)
+
+# Configuracoes banco de dados
+host = os.getenv('host')
+user = os.getenv('user')
+password = os.getenv('password')
+port = os.getenv('port')
+banco = os.getenv('banco')
 
 # Registro de blueprints Flask
 app.register_blueprint(auth_bp, url_prefix='/auth')
